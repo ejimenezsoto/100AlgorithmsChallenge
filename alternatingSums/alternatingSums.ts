8598563f26c2a044cdaa8ca6ffb9feb915258d0e
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-12-29 17:54:36 -0800

    ...boxBlur

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,15 @@
 export function alternatingSums(a: number[]): number[] {
+    let evenSum = 0;
+    let oddSum = 0;
 
+    a.forEach((num,index) => {
+        if(index % 2 === 0){
+            evenSum += num
+        } else{
+            oddSum += num
+        }
+    })
+    return [evenSum,oddSum]
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,10 @@
 export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-
+    inputArray.forEach((num, index) => {
+        if(num === elemToReplace){
+            inputArray[index] = substitutionElem
+        }
+    })
+    return inputArray
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
